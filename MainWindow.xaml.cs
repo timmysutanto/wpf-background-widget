819a593bfdcbf39d1ab40a835f3abf0019e9e32a
timothy1 <timothy1@alto.id>
2025-12-31 18:29:23 +0700

    Start at bottom right corner

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -10,7 +10,7 @@ public partial class MainWindow : Window
     private int _count = 0;
 
     // Win32 constants for desktop pinning
-    private static readonly IntPtr HWND_BOTTOM = new(1);
+    private static readonly IntPtr HWND_BOTTOM = new(9999);
     private const uint SWP_NOSIZE = 0x0001;
     private const uint SWP_NOMOVE = 0x0002;
     private const uint SWP_NOACTIVATE = 0x0010;
@@ -21,13 +21,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        this.Top = SystemParameters.WorkArea.Height - this.Height;
+        this.Left = SystemParameters.WorkArea.Width - this.Width;
         this.Loaded += (s, e) => PinToDesktop();
     }
 
     private void PinToDesktop()
     {
         IntPtr hWnd = new WindowInteropHelper(this).Handle;
-        SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+        SetWindowPos(hWnd, HWND_BOTTOM, Convert.ToInt32(this.Left), Convert.ToInt32(this.Top), Convert.ToInt32(this.Left), Convert.ToInt32(this.Top), SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
     }
 
     private void Increment_Click(object sender, RoutedEventArgs e)
