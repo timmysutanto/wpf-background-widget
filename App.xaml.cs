425a90be89884852575d8e8e1ab1c1dfcabf4540
timothy1 <timothy1@alto.id>
2025-12-31 17:56:17 +0700

    Init

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_background_widget;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace CounterWidget;
+
+public partial class MainWindow : Window
+{
+    private int _count = 0;
+
+    // Win32 constants for desktop pinning
+    private static readonly IntPtr HWND_BOTTOM = new(1);
+    private const uint SWP_NOSIZE = 0x0001;
+    private const uint SWP_NOMOVE = 0x0002;
+    private const uint SWP_NOACTIVATE = 0x0010;
+
+    [DllImport("user32.dll")]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.Loaded += (s, e) => PinToDesktop();
+    }
+
+    private void PinToDesktop()
+    {
+        IntPtr hWnd = new WindowInteropHelper(this).Handle;
+        SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE | SWP_NOACTIVATE);
+    }
+
+    private void Increment_Click(object sender, RoutedEventArgs e)
+    {
+        _count++;
+        CounterDisplay.Text = _count.ToString();
+    }
+
+    private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left) DragMove();
+    }
+
+    private void Reset_Click(object sender, RoutedEventArgs e) => CounterDisplay.Text = (_count = 0).ToString();
+    private void Exit_Click(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+}
