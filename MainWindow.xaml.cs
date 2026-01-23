f14dcbba56940d63c72ef4dac15d56c255d0f7f6
timmysutanto <timmysutanto@gmail.com>
2026-01-23 17:24:26 +0700

    Add GetRandomCardNumberSuit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,17 @@ public partial class MainWindow : Window
     private const uint SWP_NOSIZE = 0x0001;
     private const uint SWP_NOMOVE = 0x0002;
     private const uint SWP_NOACTIVATE = 0x0010;
+    private static readonly List<string> CardSuits = new List<string>
+    {
+        "club", "spade", "heart", "heart"
+    };
+    private static readonly List<string> CardNumbers = new List<string>
+    {
+        "2", "3", "4", "5", "6",  "7", "8", "9", "10", "J", "Q", "K", "A"
+    };
+
+    private static List<string> CardsOnTable = new List<string>
+    { };
 
     [DllImport("user32.dll")]
     private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
@@ -34,7 +45,7 @@ public partial class MainWindow : Window
     
     private void Load_Card(object sender, RoutedEventArgs e)
     {
-        LoadImage("pack://application:,,,/Resources/Cards/club_2.png");
+        LoadImage("pack://application:,,,/Resources/Cards/" + GetRandomCardNumberSuit() + ".png");
     }
 
     private void Window_MouseDown(object sender, MouseButtonEventArgs e)
@@ -61,5 +72,25 @@ public partial class MainWindow : Window
         
         Dealer1Canvas.Children.Clear();
         Dealer1Canvas.Children.Add(img);
+    }
+
+    private string GetRandomCardNumberSuit()
+    {
+        bool found = false;
+        string chosenCardSuit;
+        string chosenCardNumber;
+        string result="";
+        while (!found)
+        {
+            chosenCardSuit = Random.Shared.GetItems(CardSuits.ToArray(), 1)[0];
+            chosenCardNumber = Random.Shared.GetItems(CardNumbers.ToArray(), 1)[0];
+            result = chosenCardSuit + "_" + chosenCardNumber;
+            if (!CardsOnTable.Contains(result))
+            {
+                CardsOnTable.Add(result);
+                return result;
+            }
+        }
+        return result;
     }
 }
